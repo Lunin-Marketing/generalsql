737fd45a7a3e1f925d8ddf3f8548c0a4364ca389
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-08-17 20:36:29 +0000

    Adding previous week tables and xf for funnel report WOW

---
+++ models/wow/funnel_report_previous_week_sqos.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table') }}
+
+WITH previous_week AS (
+SELECT
+week
+FROM "acton".dbt_actonmarketing.date_base_xf
+WHERE day=CURRENT_DATE-7
+), base AS (
+SELECT DISTINCT
+sqo_source_xf.opportunity_id AS sqo_id,
+sqo_source_xf.discovery_date AS sqo_date
+FROM "acton".dbt_actonmarketing.sqo_source_xf 
+LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
+sqo_source_xf.discovery_date=date_base_xf.day
+LEFT JOIN previous_week ON 
+date_base_xf.week=previous_week.week
+WHERE previous_week.week IS NOT null
+)
+
+SELECT *
+FROM base
---
+++ models/wow/funnel_report_previous_week_won.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table') }}
+
+WITH previous_week AS (
+SELECT
+week
+FROM "acton".dbt_actonmarketing.date_base_xf
+WHERE day=CURRENT_DATE-7
+), base AS (
+SELECT DISTINCT
+opp_sales_source_xf.opportunity_id AS won_id,
+opp_sales_source_xf.close_date AS won_date
+FROM "acton".dbt_actonmarketing.opp_sales_source_xf 
+LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
+opp_sales_source_xf.close_date=date_base_xf.day
+LEFT JOIN previous_week ON 
+date_base_xf.week=previous_week.week
+WHERE previous_week.week IS NOT null
+)
+
+SELECT *
+FROM base
