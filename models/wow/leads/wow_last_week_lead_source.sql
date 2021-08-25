0ff06c9ff159be009afdbf4c4dff5d10c76b63d2
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-08-25 22:30:26 +0000

    Updating wow last week lead table

--- models/wow/leads/wow_last_week_lead_source.sql
+++ models/wow/leads/wow_last_week_lead_source.sql
@@ -9,19 +9,20 @@ WHERE day=CURRENT_DATE-7
 
 SELECT
 lead_id,
-COALESCE(converted_date,created_date) AS marketing_date--,
-/*channel_lead_creation,
+created_date,
+--COALESCE(converted_date,created_date) AS marketing_date,
+channel_lead_creation,
 medium_lead_creation,
 source_lead_creation,
 country,
-company_size_rev*/
+company_size_rev
 FROM "acton".dbt_actonmarketing.lead_source_xf
 LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
-COALESCE(converted_date,created_date)=date_base_xf.day
+created_date=date_base_xf.day
 LEFT JOIN current_week ON 
 date_base_xf.week=current_week.week
 WHERE current_week.week IS NOT null
-AND COALESCE(converted_date,created_date) IS NOT null
+AND created_date IS NOT null
 AND lead_owner != '00Ga0000003Nugr' -- AO-Fake Leads
 AND email NOT LIKE '%act-on.com'
 AND lead_source = 'Marketing'
