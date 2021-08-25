3abca5c0b8cd2e86c43ca2b2a443c11fe8172e1c
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-08-25 19:48:44 +0000

    Updating AO Fake LEads to ID and created WOW LAst Week tables

---
+++ models/wow/leads/wow_last_week_lead_source.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='table') }}
+
+WITH current_week AS (
+SELECT
+week 
+FROM "acton".dbt_actonmarketing.date_base_xf
+WHERE day=CURRENT_DATE-7
+)
+
+SELECT
+lead_id,
+created_date,
+channel_lead_creation,
+medium_lead_creation,
+source_lead_creation,
+country,
+company_size_rev
+FROM "acton".dbt_actonmarketing.lead_source_xf
+LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
+lead_source_xf.created_date=date_base_xf.day
+LEFT JOIN current_week ON 
+date_base_xf.week=current_week.week
+WHERE current_week.week IS NOT null
+WHERE created_date IS NOT null
+AND lead_owner != '00Ga0000003Nugr' -- AO-Fake Leads
+AND email NOT LIKE '%act-on.com'
+AND lead_source = 'Marketing'
