d23a9f7a79c3ea55bfefc9085615a73bfa738156
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-09-03 17:14:23 +0000

    Create HR tables

---
+++ models/wow/handraisers/hr_l12w_type.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='table') }}
+
+WITH last_12_weeks AS (
+SELECT DISTINCT
+week 
+FROM "acton".dbt_actonmarketing.date_base_xf
+WHERE day BETWEEN CURRENT_DATE-84 AND CURRENT_DATE-7
+)
+
+SELECT
+lead_id,
+created_date,
+offer_asset_type_lead_creation,
+lead_status
+FROM "acton".dbt_actonmarketing.lead_source_xf
+LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
+lead_source_xf.created_date=date_base_xf.day
+LEFT JOIN last_12_weeks ON 
+date_base_xf.week=last_12_weeks.week
+WHERE last_12_weeks.week IS NOT null
+AND created_date IS NOT null
+AND lead_owner != '00Ga0000003Nugr' -- AO-Fake Leads
+AND email NOT LIKE '%act-on.com'
+AND LOWER(is_hand_raiser) = 'true'
+AND LOWER(lead_status) NOT IN ('current customer','partner','bad data','no fit')
