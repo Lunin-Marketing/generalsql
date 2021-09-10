615d18af9bc878445c458286ed9422325ee1d617
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-09-10 19:48:14 +0000

    Create wow_disco tables

--- models/wow/disco calls/wow_disco_pastdue.sql
+++ models/wow/disco calls/wow_disco_pastdue.sql
@@ -1,19 +1,22 @@
 {{ config(materialized='table') }}
 
-WITH base AS (
+WITH  base AS (
 
 SELECT
 opportunity_id,
-owner_id
+opportunity_name,
+owner_id,
+discovery_call_scheduled_datetime
 FROM "acton".dbt_actonmarketing.opp_source_xf
 WHERE 1=1
 AND type = 'New Business'
-AND discovery_call_scheduled_datetime IS null
+AND discovery_call_scheduled_datetime IS NOT null
 AND discovery_date IS null
+AND opportunity_status != 'Rescheduling Meeting'
 AND is_closed != 'true'
 AND is_won != 'true'
 AND is_deleted != 'true'
-
+AND discovery_call_scheduled_datetime < CURRENT_DATE
 )
 
 SELECT
