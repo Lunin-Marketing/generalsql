277cbe05369f92ed9ece8673a689542960ab9fa6
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-10-05 14:23:12 +0000

    Updating lead_mql_source

--- models/lead_mql_source_xf.sql
+++ models/lead_mql_source_xf.sql
@@ -9,9 +9,10 @@ is_hand_raiser,
 mql_created_date,
 mql_most_recent_date,
 lead_status,
-country
+country,
+lead_owner
 FROM "acton".dbt_actonmarketing.lead_source_xf
 WHERE mql_created_date IS NOT null
-AND lead_owner != 'AO-Fake Leads'
+AND lead_owner != '00Ga0000003Nugr' -- AO-Fake Leads
 AND email NOT LIKE '%act-on.com'
 AND lead_source = 'Marketing'
--- models/wow/lost/wow_lost_l12w_all.sql
+++ models/wow/lost/wow_lost_l12w_all.sql
@@ -6,7 +6,22 @@ week
 FROM "acton".dbt_actonmarketing.date_base_xf
 WHERE day BETWEEN CURRENT_DATE-84 AND CURRENT_DATE-7
 
-), final AS (
+),lost_final AS (
+SELECT
+close_date,
+opp_channel_lead_creation,
+opp_lead_source,
+opportunity_id,
+acv_deal_size_usd
+FROM "acton".dbt_actonmarketing.opp_source_xf
+LEFT JOIN "acton".dbt_actonmarketing.account_source_xf ON
+opp_source_xf.account_id=account_source_xf.account_id
+WHERE close_date IS NOT null
+AND discovery_date IS NOT null
+AND type = 'New Business'
+AND stage_name IN ('Closed – Lost No Resources / Budget','Closed – Lost Not Ready / No Decision','Closed – Lost Product Deficiency','Closed - Lost to Competitor')
+
+),final AS (
     
 SELECT
 date_base_xf.week,
@@ -14,18 +29,12 @@ opp_channel_lead_creation,
 opp_lead_source,
 COUNT(opportunity_id) AS won,
 SUM(acv_deal_size_usd) AS acv
-FROM "acton".dbt_actonmarketing.opp_source_xf
-LEFT JOIN "acton".dbt_actonmarketing.account_source_xf ON
-opp_source_xf.account_id=account_source_xf.account_id
+FROM last_12_weeks
 LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
-opp_source_xf.close_date=date_base_xf.day
-LEFT JOIN last_12_weeks ON 
 date_base_xf.week=last_12_weeks.week
+LEFT JOIN lost_final ON 
+lost_final.close_date=date_base_xf.day
 WHERE last_12_weeks.week IS NOT null
-AND close_date IS NOT null
-AND discovery_date IS NOT null
-AND type = 'New Business'
-AND stage_name IN ('Closed – Lost No Resources / Budget','Closed – Lost Not Ready / No Decision','Closed – Lost Product Deficiency','Closed - Lost to Competitor')
 GROUP BY 1,2,3
 
 )
