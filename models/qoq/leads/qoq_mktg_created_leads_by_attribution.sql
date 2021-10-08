26bb4264f17b5c6d4d85012bbdecc1b5df7ba794
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2021-10-08 20:45:35 +0000

    Updating MM tables to be MM and Ent

---
+++ models/qoq/leads/qoq_mktg_created_leads_by_attribution.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='table') }}
+
+WITH last_quarter AS (
+SELECT DISTINCT
+quarter 
+FROM "acton".dbt_actonmarketing.date_base_xf
+WHERE day BETWEEN CURRENT_DATE-90 AND CURRENT_DATE
+)
+
+SELECT
+lead_id,
+last_quarter.quarter,
+--marketing_created_date AS created_date,
+--COALESCE(converted_date,created_date) AS marketing_date,
+channel_lead_creation,
+medium_lead_creation,
+source_lead_creation,
+country,
+company_size_rev 
+FROM "acton".dbt_actonmarketing.lead_source_xf
+LEFT JOIN "acton".dbt_actonmarketing.date_base_xf ON
+marketing_created_date=date_base_xf.day
+LEFT JOIN last_quarter ON 
+date_base_xf.quarter=last_quarter.quarter
+WHERE last_quarter.quarter IS NOT null
+AND marketing_created_date IS NOT null
+AND lead_owner != '00Ga0000003Nugr' -- AO-Fake Leads
+AND email NOT LIKE '%act-on.com'
+AND lead_source = 'Marketing'
