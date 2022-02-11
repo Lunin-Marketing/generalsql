1a585f875ca1484e755819df24ed3610f910297f
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2022-02-11 16:29:33 +0000

    Update source tables with Fivetran data

--- models/contact_source_xf.sql
+++ models/contact_source_xf.sql
@@ -1,112 +1,114 @@
 {{ config(materialized='table') }}
+
 WITH base AS (
+
 SELECT *
 FROM "acton".salesforce."contact"
 
 ), final AS (
+
 SELECT 
-id AS contact_id,
-is_deleted,
-account_id,
-first_name,
-mailing_postal_code,
-mailing_country,
-email,
-title,
-lead_source,
-DATE_TRUNC('day',created_date)::Date AS created_date,
-last_modified_date,
-system_modstamp AS systemmodstamp,
-current_customer_reference_c AS is_current_customer,
-no_longer_with_company_c AS is_no_longer_with_company,
-hand_raiser_c AS is_hand_raiser,
-mql_created_date_c AS mql_created_date,
-mql_most_recent_date_c AS mql_most_recent_date,
-contact_role_c AS contact_role,
-primary_contact_c AS is_primary_contact,
-ft_offerAsset_type_c AS offer_asset_type_first_touch,
-ft_offer_asset_subtype_c AS offer_asset_subtype_first_touch,
-contact_status_c AS contact_status,
-ft_utm_channel_c AS channel_first_touch,
-lt_utm_channel_c AS channel_last_touch,
-lt_utm_medium_c AS medium_last_touch,
-lt_utm_source_c AS source_last_touch,
-lt_utm_campaign_c AS campaign_last_touch,
-channel_lead_creation_c AS channel_lead_creation,
-content_lead_creation_c AS content_lead_creation,
-campaign_lead_creation_c AS campaign_lead_creation,
-ft_offer_asset_topic_c AS offer_asset_topic_first_touch,
-ft_offer_asset_name_c AS offer_asset_name_first_touch,
-lc_offer_asset_type_c AS offer_asset_type_lead_creation,
-lc_offer_asset_subtype_c AS offer_asset_subtype_lead_creation,
-lc_offer_asset_topic_c AS offer_asset_topic_lead_creation,
-lc_offer_asset_name_c AS offer_asset_name_lead_creation,
-lt_offer_asset_type_c AS offer_asset_type_last_touch,
-lt_offer_asset_subtype_c AS offer_asset_subtype_last_touch,
-lt_offer_asset_name_c AS offer_asset_name_last_touch,
-lt_offer_asset_topic_c AS offer_asset_topic_last_touch,
-renewal_contact_c AS is_renewal_contact, --verify data type
-account_owner_email_c AS account_owner_email,
-account_csm_email_c AS account_csm_email,
-ft_subchannel_c AS subchannel_first_touch,
-lt_subchannel_c AS subchannel_last_touch,
-lc_subchannel_c AS subchannel_lead_creation,
-x_9883_lead_score_c AS lead_score,
-status_reason_c AS status_reason,
-marketing_lead_creation_date_c AS marketing_created_date,
-current_map_c AS current_ma,
-account_lookup_c AS account_lookup,
-ft_utm_campaign_c AS campaign_first_touch,
-ft_utm_medium_c AS medium_first_touch,
-ft_utm_source_c AS source_first_touch,
--- AS mailing_postal_code,
--- AS mailing_country,
--- AS last_name, 
--- AS owner_id,
--- "Medium_Lead_Creation__c" AS medium_lead_creation,
--- "Source_Lead_Creation__c" AS source_lead_creation,
--- "Account_Owner_ID__c" AS account_owner_id, 
--- "Account_Owner__c" AS account_owner, --join to account
--- "Engagement_Level__c" AS engagement_level,
--- "Firmographic_Demographic_Lead_Grade__c" AS firmographic_demographic_lead_grade,
--- "Firmographic_Demographic_Lead_Score__c" AS firmographic_demographic_lead_score,
--- "Was_a_Handraiser_Lead__c" AS was_a_handraiser_lead,
--- "LeadID_Converted_From__c" AS lead_id_converted_from,
--- "Account_CSM__c" AS account_csm,-- join to account
--- "Account_CSM_ID__c" AS account_csm_id,-- join to account
--- "Account_SDR_Photo__c" AS account_sdr_photo,-- join to account
--- "Account_SDR_Phone__c" AS account_sdr_phone,-- join to account
--- "Account_SDR_Calendly__c" AS account_sdr_calendly,-- join to account
--- "Account_SDR_Title__c" AS account_sdr_title,-- join to account
--- "Account_Deliverability_Consultant_Email__c" AS account_deliverability_consultant_email,-- join to account
--- "Account_Deliverability_Consultant__c" AS account_deliverability_consultant,-- join to account
--- "Account_SDR_Full_Name__c" AS account_sdr_full_name,-- join to account
--- "Account_SDR_Email__c" AS account_sdr_email,-- join to account
--- "Owner_Email__c" AS owner_email, --join to user
--- annual_revenue, --join to account
-CASE WHEN annual_revenue <= 49999999 THEN 'SMB'
-     WHEN annual_revenue > 49999999 AND annual_revenue <= 499999999 THEN 'Mid-Market'
-     WHEN annual_revenue > 499999999 THEN 'Enterprise'
+base.id AS contact_id,
+base.is_deleted,
+base.account_id,
+base.first_name,
+base.mailing_postal_code,
+base.mailing_country,
+base.email,
+base.title,
+base.lead_source,
+DATE_TRUNC('day',base.created_date)::Date AS created_date,
+base.last_modified_date,
+base.system_modstamp AS systemmodstamp,
+base.current_customer_reference_c AS is_current_customer,
+base.no_longer_with_company_c AS is_no_longer_with_company,
+base.hand_raiser_c AS is_hand_raiser,
+base.mql_created_date_c AS mql_created_date,
+base.mql_most_recent_date_c AS mql_most_recent_date,
+base.contact_role_c AS contact_role,
+base.primary_contact_c AS is_primary_contact,
+base.ft_offer_asset_type_c AS offer_asset_type_first_touch,
+base.ft_offer_asset_subtype_c AS offer_asset_subtype_first_touch,
+base.contact_status_c AS contact_status,
+base.ft_utm_channel_c AS channel_first_touch,
+base.lt_utm_channel_c AS channel_last_touch,
+base.lt_utm_medium_c AS medium_last_touch,
+base.lt_utm_source_c AS source_last_touch,
+base.lt_utm_campaign_c AS campaign_last_touch,
+base.channel_lead_creation_c AS channel_lead_creation,
+base.content_lead_creation_c AS content_lead_creation,
+base.campaign_lead_creation_c AS campaign_lead_creation,
+base.ft_offer_asset_topic_c AS offer_asset_topic_first_touch,
+base.ft_offer_asset_name_c AS offer_asset_name_first_touch,
+base.lc_offer_asset_type_c AS offer_asset_type_lead_creation,
+base.lc_offer_asset_subtype_c AS offer_asset_subtype_lead_creation,
+base.lc_offer_asset_topic_c AS offer_asset_topic_lead_creation,
+base.lc_offer_asset_name_c AS offer_asset_name_lead_creation,
+base.lt_offer_asset_type_c AS offer_asset_type_last_touch,
+base.lt_offer_asset_subtype_c AS offer_asset_subtype_last_touch,
+base.lt_offer_asset_name_c AS offer_asset_name_last_touch,
+base.lt_offer_asset_topic_c AS offer_asset_topic_last_touch,
+base.renewal_contact_c AS is_renewal_contact, --verify data type
+base.account_owner_email_c AS account_owner_email,
+base.account_csm_email_c AS account_csm_email,
+base.ft_subchannel_c AS subchannel_first_touch,
+base.lt_subchannel_c AS subchannel_last_touch,
+base.lc_subchannel_c AS subchannel_lead_creation,
+base.x_9883_lead_score_c AS lead_score,
+base.status_reason_c AS status_reason,
+base.marketing_lead_creation_date_c AS marketing_created_date,
+base.current_map_c AS current_ma,
+base.account_lookup_c AS account_lookup,
+base.ft_utm_campaign_c AS campaign_first_touch,
+base.ft_utm_medium_c AS medium_first_touch,
+base.ft_utm_source_c AS source_first_touch,
+base.lead_id_converted_from_c AS lead_id_converted_from,
+base.was_a_handraiser_lead_c AS was_a_handraiser_lead,
+base.medium_lead_creation_c AS medium_lead_creation,
+base.source_lead_creation_c AS source_lead_creation,
+base.owner_id,
+base.firmographic_demographic_lead_score_c AS firmographic_demographic_lead_score,
+base.last_name, 
+base.data_enrich_company_name_c AS de_account_name,
+account_source_xf.account_owner_id,
+account_source_xf.account_owner_name,
+account_source_xf.annual_revenue,
+account_source_xf.de_current_crm,
+account_source_xf.de_current_ma,
+account_source_xf.csm_name AS account_csm_name,
+account_source_xf.csm_id AS account_csm_id,
+account_source_xf.sdr_phone AS account_sdr_phone,
+account_source_xf.sdr_photo AS account_sdr_photo,
+account_source_xf.sdr_calendly AS account_sdr_calendly,
+account_source_xf.sdr_title AS account_sdr_title,
+account_source_xf.sdr_full_name AS account_sdr_full_name,
+account_source_xf.sdr_email AS account_sdr_email,
+account_source_xf.account_deliverability_consultant_email,
+account_source_xf.account_deliverability_consultant,
+user_source_xf.user_email AS owner_email,
+CASE WHEN account_source_xf.annual_revenue <= 49999999 THEN 'SMB'
+     WHEN account_source_xf.annual_revenue > 49999999 AND account_source_xf.annual_revenue <= 499999999 THEN 'Mid-Market'
+     WHEN account_source_xf.annual_revenue > 499999999 THEN 'Enterprise'
      END AS company_size_rev,
 CASE 
     WHEN LOWER(channel_lead_creation_c) = 'organic' THEN 'Organic'
     WHEN LOWER(channel_lead_creation_c) IS null THEN 'Unknown'
-    WHEN LOWER(channel_lead_creation_c) = 'social' AND LOWER("Medium_Lead_Creation__c") = 'social-organic' THEN 'Social - Organic'
-    WHEN LOWER(channel_lead_creation_c) = 'social' AND LOWER("Medium_Lead_Creation__c") = 'social-paid' THEN 'Paid Social'
+    WHEN LOWER(channel_lead_creation_c) = 'social' AND LOWER(medium_lead_creation_c) = 'social-organic' THEN 'Social - Organic'
+    WHEN LOWER(channel_lead_creation_c) = 'social' AND LOWER(medium_lead_creation_c) = 'social-paid' THEN 'Paid Social'
     WHEN LOWER(channel_lead_creation_c) = 'ppc' THEN 'PPC/Paid Search'
-    WHEN LOWER(channel_lead_creation_c) = 'email' AND LOWER("Source_Lead_Creation__c") like '%act-on%' THEN 'Paid Email' 
-    WHEN LOWER(channel_lead_creation_c) = 'ppl' AND LOWER("Medium_Lead_Creation__c") = 'syndication partner' THEN 'PPL'
-    WHEN LOWER(channel_lead_creation_c) IN ('prospecting','ppl') AND LOWER("Medium_Lead_Creation__c") = 'intent partner' THEN 'Intent Partners'
+    WHEN LOWER(channel_lead_creation_c) = 'email' AND LOWER(source_lead_creation_c) like '%act-on%' THEN 'Paid Email' 
+    WHEN LOWER(channel_lead_creation_c) = 'ppl' AND LOWER(medium_lead_creation_c) = 'syndication partner' THEN 'PPL'
+    WHEN LOWER(channel_lead_creation_c) IN ('prospecting','ppl') AND LOWER(medium_lead_creation_c) = 'intent partner' THEN 'Intent Partners'
     WHEN LOWER(channel_lead_creation_c) = 'event' THEN 'Events and Trade Shows'
     WHEN LOWER(channel_lead_creation_c) = 'partner' THEN 'Partners'
     ELSE 'Other'
-    END AS channel_bucket,
--- de_current_crm, --join to account
--- de_current_ma, --join to account
--- sdr
+    END AS channel_bucket
 FROM base
-LEFT JOIN "acton".dbt_actonmarketing.account_source_xf ON
-base."AccountId"=account_source_xf.account_id
+LEFT JOIN {{ref('account_source_xf')}} ON
+base.account_id=account_source_xf.account_id
+LEFT JOIN {{ref('user_source_xf')}} ON
+base.owner_id=user_source_xf.user_id
+
 )
 
 SELECT
--- models/lead_source_xf.sql
+++ models/lead_source_xf.sql
@@ -1,103 +1,107 @@
 {{ config(materialized='table') }}
+
 WITH base AS (
+
 SELECT *
-FROM "acton".salesforce."ead"
+FROM "acton".salesforce."lead"
 
 ), final AS (
-SELECT
-id AS lead_id,
-is_deleted,
-first_name,
-last_name, 
-title,
-company,
-state,
-country,
-email,
-lead_source,
-status AS lead_status,
-industry,
-annual_revenue,
-number_of_employees,
-owner_id AS lead_owner,
-is_converted,
-DATE_TRUNC('day',converted_date)::Date AS converted_date,
-converted_account_id,
-converted_contact_id,
-converted_opportunity_id,
-last_modified_date,
-DATE_TRUNC('day',created_date)::Date AS created_date,
-system_modstamp AS systemmodstamp,
-DATE_TRUNC('day',mql_created_date)::Date AS mql_created_date,
-DATE_TRUNC('day',mql_most_recent_date)::Date AS mql_most_recent_date,
-account_c AS account_id,
-no_longer_with_company_c AS no_longer_with_company,
-ft_utm_channel_c AS channel_first_touch,
-lt_utm_channel_c AS channel_last_touch,
-lt_utm_medium_c AS medium_last_touch,
-lt_utm_content_c AS content_last_touch,
-lt_utm_source_c AS source_last_touch,
-lt_utm_campaign_c AS campaign_last_touch,
-channel_lead_creation_c AS channel_lead_creation,
-medium_lead_creation_c AS medium_lead_creation,
-hand_raiser_c AS is_hand_raiser,
-ft_subchannel_c AS subchannel_first_touch,
-lt_subchannel_c AS subchannel_last_touch,
-lc_subchannel_c AS subchannel_lead_creation,
-ft_offer_asset_type_c AS offer_asset_type_first_touch,
-ft_offer_asset_subtype_c AS offer_asset_subtype_first_touch,
-ft_offer_asset_topic_c AS offer_asset_topic_first_touch,
-ft_offer_asset_name_c AS offer_asset_name_first_touch,
-lc_offer_asset_type_c AS offer_asset_type_lead_creation,
-lc_offer_asset_subtype_c AS offer_asset_subtype_lead_creation,
-lc_offer_asset_topic_c AS offer_asset_topic_lead_creation,
-lc_offer_asset_name_c AS offer_asset_name_lead_creation,
-lt_offer_asset_type_c AS offer_asset_type_last_touch,
-lt_offer_asset_subtype_c AS offer_asset_subtype_last_touch,
-lt_offer_asset_topic_c AS offer_asset_topic_last_touch,
-lt_offer_asset_name_c AS offer_asset_name_last_touch,
-lean_data_a_2_b_account_c AS lean_data_account_id,
-de_current_marketing_automation_c AS current_ma,
-de_current_crm_c AS current_crm,
-DATE_TRUNC('day',marketing_lead_creation_date_c)::Date AS marketing_created_date,
-mql_created_time_c AS mql_created_datetime,
-mql_most_recent_time_c AS mql_most_recent_datetime,
-article_14_notice_date_c AS article_14_notice_date,
-x_9883_lead_score_c AS lead_score,
-ft_utm_medium_c AS medium_first_touch,
-ft_utm_source_c AS source_first_touch,
-ft_utm_campaign_c AS campaign_first_touch,
-channel_lead_creation_c AS lead_channel_forecast,
 
+    SELECT
+        id AS lead_id,
+        is_deleted,
+        first_name,
+        last_name, 
+        title,
+        company,
+        state,
+        country,
+        email,
+        lead_source,
+        status AS lead_status,
+        industry,
+        annual_revenue,
+        number_of_employees,
+        owner_id AS lead_owner,
+        is_converted,
+        DATE_TRUNC('day',converted_date)::Date AS converted_date,
+        converted_account_id,
+        converted_contact_id,
+        converted_opportunity_id,
+        last_modified_date,
+        DATE_TRUNC('day',created_date)::Date AS created_date,
+        system_modstamp AS systemmodstamp,
+        DATE_TRUNC('day',mql_created_date_c)::Date AS mql_created_date,
+        DATE_TRUNC('day',mql_most_recent_date_c)::Date AS mql_most_recent_date,
+        account_c AS account_id,
+        no_longer_with_company_c AS no_longer_with_company,
+        ft_utm_channel_c AS channel_first_touch,
+        lt_utm_channel_c AS channel_last_touch,
+        lt_utm_medium_c AS medium_last_touch,
+        lt_utm_content_c AS content_last_touch,
+        lt_utm_source_c AS source_last_touch,
+        lt_utm_campaign_c AS campaign_last_touch,
+        channel_lead_creation_c AS channel_lead_creation,
+        medium_lead_creation_c AS medium_lead_creation,
+        hand_raiser_c AS is_hand_raiser,
+        ft_subchannel_c AS subchannel_first_touch,
+        lt_subchannel_c AS subchannel_last_touch,
+        lc_subchannel_c AS subchannel_lead_creation,
+        ft_offer_asset_type_c AS offer_asset_type_first_touch,
+        ft_offer_asset_subtype_c AS offer_asset_subtype_first_touch,
+        ft_offer_asset_topic_c AS offer_asset_topic_first_touch,
+        ft_offer_asset_name_c AS offer_asset_name_first_touch,
+        lc_offer_asset_type_c AS offer_asset_type_lead_creation,
+        lc_offer_asset_subtype_c AS offer_asset_subtype_lead_creation,
+        lc_offer_asset_topic_c AS offer_asset_topic_lead_creation,
+        lc_offer_asset_name_c AS offer_asset_name_lead_creation,
+        lt_offer_asset_type_c AS offer_asset_type_last_touch,
+        lt_offer_asset_subtype_c AS offer_asset_subtype_last_touch,
+        lt_offer_asset_topic_c AS offer_asset_topic_last_touch,
+        lt_offer_asset_name_c AS offer_asset_name_last_touch,
+        lean_data_a_2_b_account_c AS lean_data_account_id,
+        de_current_marketing_automation_c AS current_ma,
+        de_current_crm_c AS current_crm,
+        DATE_TRUNC('day',marketing_lead_creation_date_c)::Date AS marketing_created_date,
+        mql_created_time_c AS mql_created_datetime,
+        mql_most_recent_time_c AS mql_most_recent_datetime,
+        article_14_notice_date_c AS article_14_notice_date,
+        x_9883_lead_score_c AS lead_score,
+        ft_utm_medium_c AS medium_first_touch,
+        ft_utm_source_c AS source_first_touch,
+        ft_utm_campaign_c AS campaign_first_touch,
+        channel_lead_creation_c AS lead_channel_forecast,
+        email_bounced_reason,
+        legitimate_basis_c AS legitimate_basis,
+        email_bounced_date,
+        source_lead_creation_c AS source_lead_creation,
+        campaign_lead_creation_c AS campaign_lead_creation,
+        firmographic_demographic_lead_score_c AS firmographic_demographic_lead_score,
+        state,
+        do_not_contact_c AS do_not_contact,
+        form_consent_opt_in_c AS form_consent_opt_in,
+        CASE 
+            WHEN annual_revenue <= 49999999 THEN 'SMB'
+            WHEN annual_revenue > 49999999 AND annual_revenue <= 499999999 THEN 'Mid-Market'
+            WHEN annual_revenue > 499999999 THEN 'Enterprise'
+        END AS company_size_rev,
+        CASE 
+            WHEN LOWER(channel_lead_creation_c) = 'organic' THEN 'Organic'
+            WHEN LOWER(channel_lead_creation_c) IS null THEN 'Unknown'
+            WHEN LOWER(channel_lead_creation_c) = 'social' AND LOWER(medium_lead_creation_c) = 'social-organic' THEN 'Social - Organic'
+            WHEN LOWER(channel_lead_creation_c) = 'social' AND LOWER(medium_lead_creation_c) = 'social-paid' THEN 'Paid Social'
+            WHEN LOWER(channel_lead_creation_c) = 'ppc' THEN 'PPC/Paid Search'
+            WHEN LOWER(channel_lead_creation_c) = 'email' AND LOWER(source_lead_creation_c) like '%act-on%' THEN 'Paid Email' 
+            WHEN LOWER(channel_lead_creation_c) = 'ppl' AND LOWER(medium_lead_creation_c) = 'syndication partner' THEN 'PPL'
+            WHEN LOWER(channel_lead_creation_c) IN ('prospecting','ppl') AND LOWER(medium_lead_creation_c) = 'intent partner' THEN 'Intent Partners'
+            WHEN LOWER(channel_lead_creation_c) = 'event' THEN 'Events and Trade Shows'
+            WHEN LOWER(channel_lead_creation_c) = 'partner' THEN 'Partners'
+            ELSE 'Other'
+        END AS channel_bucket,
+        COALESCE(account_c,lean_data_a_2_b_account_c) AS person_account_id
+    FROM base
+    WHERE base.owner_id != '00Ga0000003Nugr' -- AO-Fake Leads
 
-"Source_Lead_Creation__c" AS source_lead_creation,
-"EmailBouncedReason" AS email_bounced_reason,
-"EmailBouncedDate" AS email_bounced_date,
-"FirmographicDemographic_Lead_Score__c" AS firmographic_demographic_lead_score,
-"Do_Not_Contact__c" AS do_not_contact,
-"Form_Consent_Opt_In__c" AS form_consent_opt_in,
-"Campaign_Lead_Creation__c" AS campaign_lead_creation,
-"Legitimate_Basis__c" AS legitimate_basis,
-CASE WHEN annual_revenue <= 49999999 THEN 'SMB'
-     WHEN annual_revenue > 49999999 AND annual_revenue <= 499999999 THEN 'Mid-Market'
-     WHEN annual_revenue > 499999999 THEN 'Enterprise'
-     END AS company_size_rev,
-CASE 
-    WHEN LOWER(channel_lead_creation) = 'organic' THEN 'Organic'
-    WHEN LOWER(channel_lead_creation) IS null THEN 'Unknown'
-    WHEN LOWER(channel_lead_creation) = 'social' AND LOWER("Medium_Lead_Creation__c") = 'social-organic' THEN 'Social - Organic'
-    WHEN LOWER(channel_lead_creation) = 'social' AND LOWER("Medium_Lead_Creation__c") = 'social-paid' THEN 'Paid Social'
-    WHEN LOWER(channel_lead_creation) = 'ppc' THEN 'PPC/Paid Search'
-    WHEN LOWER(channel_lead_creation) = 'email' AND LOWER("Source_Lead_Creation__c") like '%act-on%' THEN 'Paid Email' 
-    WHEN LOWER(channel_lead_creation) = 'ppl' AND LOWER("Medium_Lead_Creation__c") = 'syndication partner' THEN 'PPL'
-    WHEN LOWER(channel_lead_creation) IN ('prospecting','ppl') AND LOWER("Medium_Lead_Creation__c") = 'intent partner' THEN 'Intent Partners'
-    WHEN LOWER(channel_lead_creation) = 'event' THEN 'Events and Trade Shows'
-    WHEN LOWER(channel_lead_creation) = 'partner' THEN 'Partners'
-    ELSE 'Other'
-    END AS channel_bucket,
-COALESCE(account_id,lean_data_account_id) AS person_account_id
-FROM base
-WHERE base."OwnerId" != '00Ga0000003Nugr' -- AO-Fake Leads
 )
 
 SELECT 
