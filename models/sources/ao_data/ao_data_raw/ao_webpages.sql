1fc4a45fab591e7e3493d48577cec2c8d2fc419f
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2022-10-15 17:56:52 +0000

    Refoldering and cleanup

--- models/sources/ao_data/ao_data_raw/ao_webpages.sql
+++ models/sources/ao_data/ao_data_raw/ao_webpages.sql
@@ -7,18 +7,5 @@ WITH base AS (
 
 )
 
-SELECT
-    action,
-    action_time,
-    action_day,
-    cookie_id,
-    attribution_id,
-    contact_e_mail,
-    ip_address,
-    page_url,
-    referral_url,
-    visitor_type,
-    e_mail_domain,
-    unique_visitor_id,
-    url_path_info
+SELECT *
 FROM base
--- models/test/test2.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT DISTINCT
-   mql_most_recent_date
-FROM {{ref('person_source_xf')}}
--- WHERE mql_most_recent_date >= '2022-08-01'
--- AND mql_most_recent_date <= '2022-09-30'
-WHERE person_id = '0031O000030lAB9QAM'
