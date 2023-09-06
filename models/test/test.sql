90b7eb6f64cb21e53b815ff17d346f3e0e945bdc
robert.kohnke@act-on.com <robert.kohnke@act-on.com>
2023-09-06 10:19:37 +0000

    Testing

--- models/test/test.sql
+++ models/test/test.sql
@@ -1,8 +1,25 @@
 {{ config(materialized='table') }}
 
-SELECT DISTINCT
-    mql_id,
-    sqo_id  
-FROM {{ref('lead_to_cw_cohort')}}
-WHERE mql_created_date >= '2023-01-01'
-AND sqo_id IS NOT null
+WITH base AS (
+
+    SELECT DISTINCT
+        id,
+        mql_most_recent_date_c,
+        lt_utm_channel_c,
+        lt_utm_medium_c,
+        lt_utm_source_c,
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY mql_most_recent_date_c,dbt_valid_from DESC ) AS mql_row
+    FROM {{ref('sfdc_lead_snapshots')}}
+    WHERE mql_most_recent_date_c >= '2023-01-01'
+    -- AND id = '00Q5Y00002QjEqeUAF'
+
+)
+
+SELECT
+    id,
+    mql_most_recent_date_c,
+    lt_utm_channel_c,
+    lt_utm_medium_c,
+    lt_utm_source_c
+FROM base
+WHERE mql_row = 1
--- models/test/test2.sql
+++ models/test/test2.sql
@@ -1,7 +1,24 @@
 {{ config(materialized='table') }}
 
-SELECT 
-    id
-FROM {{ref('sfdc_lead_snapshots')}}
-WHERE is_deleted = true
-AND 
+WITH base AS (
+
+    SELECT DISTINCT
+        id,
+        date_time_to_working_c,
+        lt_utm_channel_c,
+        lt_utm_medium_c,
+        lt_utm_source_c,
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY date_time_to_working_c,dbt_valid_from DESC ) AS mql_row
+    FROM {{ref('sfdc_lead_snapshots')}}
+    WHERE date_time_to_working_c >= '2023-01-01'
+
+)
+
+SELECT
+    id,
+    date_time_to_working_c,
+    lt_utm_channel_c,
+    lt_utm_medium_c,
+    lt_utm_source_c
+FROM base
+WHERE mql_row = 1
